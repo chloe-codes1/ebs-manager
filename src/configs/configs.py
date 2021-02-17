3734db6416f7ee1b1a70156009ada29488df3061
chloe-codes1 <juhyun.kim@lindsey.edu>
2021-02-17 20:35:14 +0900

    Add configs

---
+++ src/configs/configs.py
@@ -0,0 +1,9 @@
+variables = {
+    'FILE_PATH': './test/samples/'
+}
+
+VOLUME_FILE = 'volumes.json'
+
+DEVEL_VPC = 'YOUR DEVEL VPC ID'
+PRODUCT_VPC = 'YOUR PRODUCT VPC ID'
+MANAGE_VPC = 'YOUR MANAGE VPC ID'
